2bba580f314399daf3f721b2762a1cc505681ccc
GabrielMtins <gabrielmartinsthe12381@gmail.com>
2025-07-01 13:15:59 -0300

    corrigido bug de frontend

--- frontend/src/pages/ProductPage.js
+++ frontend/src/pages/ProductPage.js
@@ -4,12 +4,21 @@ import './Catalogo.css';
 import Bookcard from '../components/Bookcard.js';
 import Bookpage from '../components/Bookpage.js';
 import { useCar } from '../contexts/CarContext';
+import { useNavigate } from 'react-router-dom';
 
 function ProductPage() {
 	const { currentBook, getItemCatalog } = useCar();
+	const navigate = useNavigate();
+
+	const back = async () => {
+		navigate("/catalogo");
+	};
 
 	return (
 		<div className="page">
+			<br />
+			<button className="back-button" onClick={back}> Voltar </button>
+			<br />
 			<br />
 			<Bookpage book={getItemCatalog(currentBook)} />
 		</div>
